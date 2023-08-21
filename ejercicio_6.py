a4f4451d9b974a9b014856a7f4275d4a50fdd2f7
Pans02 <140652135+Pans02@users.noreply.github.com>
2023-08-20 21:25:21 -0400

    Actualizar ejercicio_6.py

--- ejercicio_6.py
+++ ejercicio_6.py
@@ -1,3 +1,8 @@
-palabra=input("Ingrese una palabra: ")
-palabra=palabra[::-1]
-print("la palabra al reves es: ",palabra)
+while(True):
+  palabra=input("Ingrese una palabra: ")
+  if(palabra=="salir"):
+      break
+  palabra=palabra[::-1]
+  print("la palabra al reves es: ",palabra)
+  print("Si desea terminar la ejecucion del programa ingrese la palabra 'salir', si desea continuar siga ingresando palabras ")
+  
